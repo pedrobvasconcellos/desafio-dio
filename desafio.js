df166e62717e866d5cc3f4bfa022ea4dd71aefca
pedrobvasconcellos <vasconcellospedro213@gmail.com>
2024-10-29 17:39:01 -0300

    desafio DIO

---
+++ desafio.js
@@ -0,0 +1,12 @@
+let xpJogador = 3.500;
+let jogador = "Sansão";
+let nivel = "Prata"; 
+
+for(let i = 0; i < 3; i++)
+{
+    if(xpJogador > 2.000 && xpJogador < 5.001)
+    {
+        console.log("O heroi de nome " + jogador + " está no nível de " + nivel);
+    }
+}
+
